8b77d813d0906fa346f012aa0b90e3bdfb62a35b
krav maguy <flex4lease@gmail.com>
2021-07-09 18:51:33 -0500

    Added Issues Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import IssuesComponent from "./IssuesComponent";
+
+const BaseUrl = "https://api.github.com/repos/";
+const microsoft = "microsoft/TypeScript/issues";
+const facebook = "facebook/react/issues";
+const graphQl = "graphql/graphql-js/issues";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [gitIssues, setIssues] = useState({});
+  useEffect(() => {
+    const Issues = [
+      axios.get(`${BaseUrl}${graphQl}`),
+      axios.get(`${BaseUrl}${microsoft}`),
+      axios.get(`${BaseUrl}${facebook}`),
+    ];
+    Promise.all([...Issues])
+      .then((res) => {
+        const issues = res[0].data
+          .concat(res[1].data, res[2].data)
+          .sort((a, b) => {
+            const titleA = a.title;
+            const titleB = b.title;
+            return titleA < titleB ? -1 : titleA > titleB ? 1 : 0;
+          });
+        setIssues({ issues });
+      })
+      .catch((error) => {
+        console.log("err", error);
+      });
+  }, []);
+
+  const { issues } = gitIssues;
+
+  const removeIssue = (id) => {
+    const filteredRepo = issues.filter((item) => item.id !== id);
+    setIssues({ issues: filteredRepo });
+  };
+
+  return <IssuesComponent removeIssue={removeIssue} issues={issues} />;
 }
 
 export default App;
---
+++ src/IssuesComponent.js
@@ -0,0 +1,59 @@
+import { Card, Badge, Button } from "react-bootstrap";
+
+const IssuesComponent = ({ issues, removeIssue }) => {
+  return (
+    <>
+      <div>
+        {issues && issues.length > 0
+          ? issues.map((x) => {
+              const lastItem = x.repository_url.substring(
+                x.repository_url.lastIndexOf("/") + 1
+              );
+              const urlArray = x.html_url.split("/");
+              const repoUrl = urlArray.slice(0, urlArray.length - 2).join("/");
+              return (
+                <Card key={x.id} className="mb-3" style={{ margin: "10px" }}>
+                  <Card.Body>
+                    <div className="d-flex justify-content-between">
+                      <div>
+                        <Card.Title>
+                          <a href={x.html_url}>{x.title} - </a>
+                          <span className="text-muted font-weight-light">
+                            <a href={x.user.html_url}>{x.user.login}</a>
+                          </span>
+                        </Card.Title>
+                        <Card.Subtitle className="text-muted mb-2">
+                          <a href={repoUrl}>{repoUrl}</a>
+                        </Card.Subtitle>
+                      </div>
+                      <a href={x.user.html_url}>
+                        <img
+                          className="d-none d-md-block"
+                          height="50"
+                          alt={""}
+                          src={x.user.avatar_url}
+                        />
+                      </a>
+                    </div>
+                    <Card.Text>
+                      <Button
+                        onClick={() => removeIssue(x.id)}
+                        variant="primary"
+                      >
+                        Remove Issue
+                      </Button>
+                    </Card.Text>
+                  </Card.Body>
+                  <Card.Footer>
+                    <Badge variant="secondary">{lastItem}</Badge>
+                  </Card.Footer>
+                </Card>
+              );
+            })
+          : `loading`}
+      </div>
+    </>
+  );
+};
+
+export default IssuesComponent;
